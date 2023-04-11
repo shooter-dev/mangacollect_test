2b01f944abbc161fef93b506eabbb823895662d8
shooterdev <vincentbleach@gmail.com>
2023-04-11 16:35:20 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='mangacollect',
-    version='1.0.0',
-    description="connexion à l'api de mangacollect ",
-    packages=['mangacollect'],
+    name='mangacollect_test',
+    version='0.1',
+    description='connexion à l\'api de mangacollect',
+    url='https://github.com/shooter-dev/mangacollect_test',
     author='shooterdev',
     author_email='vincentbleach@gmail.com',
-    url='git@github.com:shooter-dev/mangacollect_test.git',
+    license='MIT',
+    packages=['mangacollect_test'],
+    install_requires=[
+        'requests',
+    ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
@@ -17,6 +21,5 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
     ],
 )
